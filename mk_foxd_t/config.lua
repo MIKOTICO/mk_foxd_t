259c720682628bb9886261ff833384c4a4ed912a
MIKOTICO <davidlopezyague.business@gmail.com>
2025-04-04 13:11:20 +1100

    -

---
+++ mk_foxd_t/config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.Engine = 0x8AAA0AD4 -- LEFT ALT
+
+Config.SpawnCommand = true 
+Config.Command = "spawn_foxd_t"
+Config.DeleteCommand = "delete_foxd_t"
+Config.ResetCommand = "reset_foxd_t"
